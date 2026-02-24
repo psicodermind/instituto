63aeffc201c3554ea7b0fa21c5b8a3e3c565ba83
MAlejandroR <manuelromeromiguel@gmail.com>
2026-02-24 11:18:40 +0100

    Actualizando authenticación

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -3,25 +3,26 @@
     <img class="max-h-full" src="{{asset("/images/logo.png")}}" alt="logo">
 
     <h1 class="text-5xl text-titulo">GESTION DE INSTITUTO</h1>
-    @guest
-        <div class="space-x-2">
+    <div class="space-x-2">
+        @guest
+
             <a href="{{route("login")}}">
                 <button class="btn btn-sm btn-primary">Login</button>
             </a>
             <a href="{{route("register")}}">
                 <button class="btn btn-sm btn-primary">Register</button>
             </a>
-        </div>
-    @endguest
-    @auth
-        {{auth()->user()->name}}
-        <form action="{{route("logout")}}" method="POST">
-            @csrf
-            <button class="btn btn-primary" type="submit">Logout</button>
-        </form>
 
-    @endauth
+        @endguest
+        @auth
+            <span class="text-green-800 text-3xl">{{auth()->user()->name}}
+            <form action="{{route("logout")}}" method="POST">
+                @csrf
+                <button class="btn btn-primary" type="submit">Logout</button>
+            </form>
 
+        @endauth
+    </div>
 
 </header>
 
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,4 +1,5 @@
 <x-layouts.layout>
+    @guest
     <div
         class="hero min-h-full"
         style="background-image: url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp);"
@@ -17,4 +18,21 @@
             </div>
         </div>
     </div>
+    @endguest
+    @auth
+            <div class="card bg-base-100 image-full w-96 shadow-sm">
+                <figure>
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">Card Title</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        @endguest
 </x-layouts.layout>
