36426da8e87f9f375a9f2e6420b791340fe04ed6
MAlejandroR <manuelromeromiguel@gmail.com>
2026-02-27 10:45:30 +0100

    con mobile first y peer

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -1,5 +1,7 @@
-<header class="h-header bg-header flex flex-row justify-between items-center p-3">
-<h1>{{__("EX1")}}</h1>
+{{--Diseño pantalla grande no móvil--}}
+<header class="hidden lg:flex flex-row justify-between items-center p-3
+h-header bg-header
+">
     <img class="max-h-full" src="{{asset("/images/logo.png")}}" alt="logo">
 
     <h1 class="text-5xl text-titulo">{{__("GESTION DE INSTITUTO")}}</h1>
@@ -7,7 +9,7 @@
         <form action="{{route("set_lang")}}" method="POST">
             @csrf
             <select name="lang" id="lang"
-            onchange="this.form.submit()">
+                    onchange="this.form.submit()">
                 {{--            HAcer que el option sea un submit--}}
                 <option selected disabled>{{__("Selecciona idioma")}}</option>
                 @foreach(config("langs") as $lang => $detail)
@@ -34,6 +36,55 @@
 
         @endauth
     </div>
+
+</header>
+
+{{--Diseño pantalla móvil--}}
+<header class="lg:hidden flex flex-row justify-center items-center p-5 space-x-5
+ bg-header
+">
+    <img class="max-h-full w-48 " src="{{asset("/images/logo.png")}}" alt="logo">
+
+    <div class=" flex flex-col space-y-3 ">
+
+        @guest
+            <div class="relative">
+                <label for="menu_login" class="fixed inset-0 bg-black/40
+                            opacity-0 pointer-events-none
+                            peer-checked:opacity-100
+                            peer-checked:pointer-events-auto
+                            transition-opacity duration-300
+                    "></label>
+
+                <label for="menu_login">
+                    <img class="w-6 h-6 " src="{{asset("/images/burguer.png")}}" alt="burguer button">
+                </label>
+                <input type="checkbox" class="peer sr-only" id="menu_login">
+                <div class="hidden peer-checked:flex  flex-col  absolute left-8 ">
+                    <a href="{{route("login")}}">
+                        <button class="btn btn-sm btn-primary">{{__("Login")}}</button>
+                    </a>
+                    <a href="{{route("register")}}">
+                        <button class="btn btn-sm btn-primary">{{__("Register")}}</button>
+                    </a>
+                </div>
+            </div>
+    @endguest
+        <div class="relative">
+            <label for="menu_lang">
+                <img class="w-6 h-6 " src="{{asset("/images/lang.png")}}" alt="burguer button">
+            </label>
+            <input type="checkbox" class="peer sr-only" id="menu_lang">
+            <form action="{{route("set_lang")}}" method="POST" class="hidden peer-checked:flex flex-col absolute left-6 top-0 bg-white p-2">
+                @csrf
+
+                    @foreach(config("langs") as $lang => $detail)
+                        <button type="submit" value="{{$lang}}">{{$detail['flag']}}  </button>
+                    @endforeach
+                </select>
+            </form>
+
+        </div>
 
 </header>
 
--- resources/views/components/layouts/nav.blade.php
+++ resources/views/components/layouts/nav.blade.php
@@ -1,7 +1,7 @@
-<nav class="h-nav bg-nav flex flex-row
+<nav class="h-nav  bg-nav flex flex-col lg:flex-row
 px-5 justify-start items-center space-x-2 ">
-    <a href="{{route("main")}}" class="btn btn-glass">About</a>
-    <a href="/noticias" class="btn btn-glass text-header">Noticias</a>
-    <a href="/alumnos" class="btn btn-glass">Alumnos</a>
+    <a href="{{route("main")}}" class="btn btn-glass w-screen lg:w-45">About</a>
+    <a href="/noticias" class="btn btn-glass text-header w-screen lg:w-full">Noticias</a>
+    <a href="/alumnos" class="btn btn-glass w-screen lg:w-full">Alumnos</a>
 </nav>
 
