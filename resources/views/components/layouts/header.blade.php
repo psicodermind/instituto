be5037f86d6022186910f3d96f86d4abdfe7c0ce
MAlejandroR <manuelromeromiguel@gmail.com>
2026-03-14 18:03:26 +0100

    Añadiendo pagination

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -2,21 +2,10 @@
 <header class="hidden lg:flex flex-row justify-between items-center p-3
 h-header bg-header
 ">
-    <img class="max-h-full" src="{{asset("/images/logo.png")}}" alt="logo">
+    <img  class="max-h-full w-46" src="{{asset("/images/logo.png")}}" alt="logo">
 
     <h1 class=" text-3xl xl:text-5xl text-titulo">{{__("GESTION DE INSTITUTO")}}</h1>
-    <div class="space-x-2">
-        <form action="{{route("set_lang")}}" method="POST">
-            @csrf
-            <select name="lang" id="lang"
-                    onchange="this.form.submit()">
-                {{--            HAcer que el option sea un submit--}}
-                <option selected disabled>{{__("Selecciona idioma")}}</option>
-                @foreach(config("langs") as $lang => $detail)
-                    <option value="{{$lang}}">{{$detail['name']}}&nbsp&nbsp&nbsp {{$detail['flag']}}  </option>
-                @endforeach
-            </select>
-        </form>
+    <div class="space-x-2 flex flex-row ">
         @guest
 
             <a href="{{route("login")}}">
@@ -28,13 +17,27 @@ h-header bg-header
 
         @endguest
         @auth
-            <span class="text-green-800 text-3xl">{{auth()->user()->name}}
+            <span class="text-green-800 text-xl">{{auth()->user()->name}}
             <form action="{{route("logout")}}" method="POST">
                 @csrf
                 <button class="btn btn-primary" type="submit">Logout</button>
             </form>
 
         @endauth
+                <form action="{{route("set_lang")}}" method="POST">
+            @csrf
+            <select class="w-24" name="lang" id="lang"
+                    onchange="this.form.submit()">
+                {{--            HAcer que el option sea un submit--}}
+                <option disabled>{{__("Selecciona idioma")}}</option>
+                @foreach(config("langs") as $lang => $detail)
+                    <option
+                        {{$lang== app()->getLocale()? "selected" : ""}}
+                        value="{{$lang}}">{{$detail['name']}} {{$detail['flag']}}  </option>
+                @endforeach
+            </select>
+        </form>
+
     </div>
 
 </header>
